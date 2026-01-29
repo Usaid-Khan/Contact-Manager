b5e2d798374e36a8a6284069c81ec9475bdabeac
Usaid-Khan <usaid1521@gmail.com>
2026-01-29 13:47:15 +0500

    Added REST endpoints

---
+++ backend/app/src/main/java/com/sclm/app/controller/ContactController.java
@@ -0,0 +1,46 @@
+package com.sclm.app.controller;
+
+import com.sclm.app.entity.Contact;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/contacts")
+public class ContactController {
+    @PostMapping("/{username}")
+    public ResponseEntity<Object> createContact(@RequestBody Contact contact, @PathVariable String username) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity<?> getAllContactsOfUser(@PathVariable String username) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getContactById(@PathVariable Long id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/{username}/{id}")
+    public ResponseEntity<?> updateContact(@PathVariable Long id, @RequestBody Contact newContact, @PathVariable String userName) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{username}/{id}")
+    public ResponseEntity<?> deleteContact(@PathVariable Long id, @PathVariable String username) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{title}")
+    public ResponseEntity<?> searchContactByTitle(@PathVariable String title) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/search/{firstName}/{lastName}")
+    public ResponseEntity<?> searchContactByName(@PathVariable String firstName, @PathVariable String lastName) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
