edd9a43b9f5939d0ecb20036fd9e43f60e67fd9b
Usaid-Khan <usaid1521@gmail.com>
2026-01-29 15:58:13 +0500

    Implemented Jpa Repositories

---
+++ backend/app/src/main/java/com/sclm/app/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.sclm.app.repository;
+
+import com.sclm.app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByPhoneNumber(String phoneNumber);
+}
